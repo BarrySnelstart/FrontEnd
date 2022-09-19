9257bfd358fdebd82017889133ef2dcc53506429
Barry Kramer <bkramer@snelstart.nl>
2022-09-19 13:55:49 +0200

    opdracht variable

--- variables_.js
+++ variables_.js
@@ -8,39 +8,39 @@ const start = "Let's go!";
 // 1. Declareer een variabele om jouw naam in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
 const name = "Barry";
-console.log(name);
-
 
 // ==========================================
 // 2. Declareer een variabele om de kleur van jouw ogen in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
-
+const eyeColor = "blauw"
 
 // ==========================================
 // 3. Declareer een variabele om jouw leeftijd in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
+let age = 48
 
 // ==========================================
 // 4. Declareer een variabele om jouw beroep in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
+let beroep = "Student"
 
 // ==========================================
 // 5. Declareer een variabele om jouw schoenmaat in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
-
+const shoeSize = 43
 
 // ==========================================
 // 6. Declareer een variabele om in op te slaan of je een partner hebt of niet. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
+let partner = true;
 
 // ==========================================
 // 7. Declareer een variabele om het aantal kinderen in op te slaan. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
+let numberOfChildren = 0;
 
 // ==========================================
 // 8. Gebruik jouw variabelen nu om hun waardes te loggen in de terminal. Klopt het allemaal?
 // ==========================================
+console.log("mijn naam is " + name + " mijn ogen zijn " +eyeColor +" van kleur, en mijn leeftijd is " +age + " jaren oud. Mijn huidige beroep is " +beroep +
+    " schoenmaat van :" +shoeSize + " en mijn relatie status is " +partner + " en we hebben" +numberOfChildren +" kinderen");
